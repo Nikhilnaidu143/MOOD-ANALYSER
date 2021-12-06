8c656d137bc926eb6fba54bd9767f13644e64395
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-06 07:36:28 +0530

    UC-1:- Analysed mood i.e., Happy or Sad.

--- src/com/mood/analyser/MoodAnalyser.java
+++ src/com/mood/analyser/MoodAnalyser.java
@@ -5,10 +5,10 @@ public class MoodAnalyser {
 	 * UC-1:- Given a Message, ability to analyse and respond Happy or Sad Mood.
 	 **/
 	public String analyseMood(String input_message) {
-		if (input_message.contains("happy")) {
-			return "HAPPY";
-		} else {
+		if (input_message.contains("Sad")) {
 			return "SAD";
+		} else {
+			return "HAPPY";
 		}
 	}
 }
--- src/com/mood/analyser/MoodAnalyserTest.java
+++ src/com/mood/analyser/MoodAnalyserTest.java
@@ -13,17 +13,19 @@ public class MoodAnalyserTest {
 		moodAnalyser = new MoodAnalyser();
 	}
 
-	@Test
-	public void analysing_mood_when_happy_should_return_happy() {
-		String input_message = "i am in happy mood";
-		String currentMood = moodAnalyser.analyseMood(input_message);
-		assertEquals("HAPPY", currentMood);
-	}
-
+	/** T.C-1.1:- Given “I am in Sad Mood” message Should Return SAD. **/
 	@Test
 	public void analysing_mood_when_sad_should_return_sad() {
-		String input_message = "i am in sad mood";
+		String input_message = "I am in Sad Mood";
 		String currentMood = moodAnalyser.analyseMood(input_message);
 		assertEquals("SAD", currentMood);
+	}
+
+	/** T.C-1.2:- Given “I am in Any Mood” message Should Return HAPPY. **/
+	@Test
+	public void analysing_mood_when_happy_should_return_happy() {
+		String input_message = "I am in Any Mood";
+		String currentMood = moodAnalyser.analyseMood(input_message);
+		assertEquals("HAPPY", currentMood);
 	}
 }
