5654b8edb2fccf4dace40fbd6593c7bb742e1939
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-06 07:09:44 +0530

    START:- Displayed welcome message.

---
+++ src/com/mood/analyser/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package com.mood.analyser;
+
+public class MoodAnalyser {
+	public static void main(String[] args) {
+		/** Displayed welcome message **/
+		System.out.println("-----------WELCOME TO MOOD ANALYSER-----------");
+	}
+}
