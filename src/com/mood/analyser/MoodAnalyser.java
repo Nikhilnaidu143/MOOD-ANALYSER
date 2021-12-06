160293ea95ed4bc87e8baa949f63befc7343eaab
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-06 08:46:39 +0530

    UC-2:- Handled invalid mood(Null).

--- src/com/mood/analyser/MoodAnalyser.java
+++ src/com/mood/analyser/MoodAnalyser.java
@@ -4,8 +4,7 @@ public class MoodAnalyser {
 	/**
 	 * UC-1(REFACTOR) :- Refactor the code to take the mood message in Constructor.
 	 **/
-
-	String input_message;
+	private String input_message;
 
 	// Default constructor.
 	public MoodAnalyser() {
@@ -17,10 +16,15 @@ public class MoodAnalyser {
 		this.input_message = input_message;
 	}
 
+	/** UC-2 :- Handle Exception if User Provides Invalid Mood. **/
 	public String analyseMood() {
-		if (input_message.contains("Sad")) {
-			return "SAD";
-		} else {
+		try {
+			if (input_message.contains("Sad")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException ex) {
 			return "HAPPY";
 		}
 	}
--- src/com/mood/analyser/MoodAnalyserTest.java
+++ src/com/mood/analyser/MoodAnalyserTest.java
@@ -1,11 +1,10 @@
 package com.mood.analyser;
 
 import static org.junit.Assert.assertEquals;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
+	static MoodAnalyser moodAnalyser;
 
 	/**
 	 * Repeat T.C-1.1:- Given “I am in Sad Mood” message in Constructor Should
@@ -13,9 +12,17 @@ public class MoodAnalyserTest {
 	 **/
 	@Test
 	public void analysing_mood_when_sad_should_return_sad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood.");
+		moodAnalyser = new MoodAnalyser("I am in Sad Mood.");
 		String currentMood = moodAnalyser.analyseMood();
 		assertEquals("SAD", currentMood);
+	}
+
+	/** T.C-2.1:- Given Null Mood Should Return Happy. **/
+	@Test
+	public void analysing_mood_when_null_should_return_happy() {
+		moodAnalyser = new MoodAnalyser(null);
+		String currentMood = moodAnalyser.analyseMood();
+		assertEquals("HAPPY", currentMood);
 	}
 
 	/**
@@ -24,7 +31,7 @@ public class MoodAnalyserTest {
 	 **/
 	@Test
 	public void analysing_mood_when_happy_should_return_happy() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood.");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
 		String currentMood = moodAnalyser.analyseMood();
 		assertEquals("HAPPY", currentMood);
 	}
