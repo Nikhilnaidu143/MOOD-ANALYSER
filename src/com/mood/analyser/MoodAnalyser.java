5902d68e450a6fd848c85316c80aff0d2683943e
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-06 08:00:51 +0530

    Refactor:- Passed mood message in constructor.

--- src/com/mood/analyser/MoodAnalyser.java
+++ src/com/mood/analyser/MoodAnalyser.java
@@ -2,9 +2,22 @@ package com.mood.analyser;
 
 public class MoodAnalyser {
 	/**
-	 * UC-1:- Given a Message, ability to analyse and respond Happy or Sad Mood.
+	 * UC-1(REFACTOR) :- Refactor the code to take the mood message in Constructor.
 	 **/
-	public String analyseMood(String input_message) {
+
+	String input_message;
+
+	// Default constructor.
+	public MoodAnalyser() {
+
+	}
+
+	// Constructor overloading.
+	public MoodAnalyser(String input_message) {
+		this.input_message = input_message;
+	}
+
+	public String analyseMood() {
 		if (input_message.contains("Sad")) {
 			return "SAD";
 		} else {
